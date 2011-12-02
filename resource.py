828494bad9b580a852bfd111cc26d53ecba6d03d
Christopher Chin <aisman64@gmail.com>
2011-12-02 17:29:20 +0800

    upload+download+gdocs integration

--- resource.py
+++ resource.py
@@ -9,17 +9,28 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
 
 class ResourcePage(webapp.RequestHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
-        resource = ''.join(['/test/serve/', resource])
-        template_values = {
+        if resource:
+            blob_instance = blobstore.BlobInfo.get(resource)
+        else:
+            self.error(404)
+        if blob_instance:
+            type = blob_instance.content_type
+            resource = ''.join([self.request.host_url, '/test/serve/', resource])
+            template_values = {
             'resource': resource,
+            'type': type,
             }
-        path = os.path.join(os.path.dirname(__file__), 'resource.html')
-        self.response.out.write(template.render(path, template_values))
-		
+            path = os.path.join(os.path.dirname(__file__), 'resource.html')
+            self.response.out.write(template.render(path, template_values))
+        else:
+            self.error(404)
+        
 def main():
     application = webapp.WSGIApplication(
           [('/resource/([^/]+)?', ResourcePage),
--- upload.py
+++ upload.py
@@ -22,13 +22,16 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
     def post(self):
         upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
         blob_info = upload_files[0]
-
+        if blob_info:
+            self.redirect('/')
+        else:
+            self.error(404)
+			
 class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
     def get(self, resource):
         resource = str(urllib.unquote(resource))
         blob_info = blobstore.BlobInfo.get(resource)
         self.send_blob(blob_info)
-
 def main():
     application = webapp.WSGIApplication(
           [('/test/upload.html', TestHandler),
