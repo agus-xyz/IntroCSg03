7fbc7103cb8d062cbf71477a5f272c14a4d93310
t00901109 <antarex.rave@gmail.com>
2011-12-07 00:38:56 +0800

    html and structure v1

---
+++ signup.py
@@ -0,0 +1,25 @@
+import os
+
+from google.appengine.ext.webapp import template
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        
+        template_values = {
+            }
+
+        path = os.path.join(os.path.dirname(__file__), 'html/signup.html')
+        self.response.out.write(template.render(path, template_values))
+
+ 
+application = webapp.WSGIApplication(
+                                     [('/signup', MainPage)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
