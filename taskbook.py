b2bdf77533d9dc5630ae2d8460880faca3ebb171
t00901109 <antarex.rave@gmail.com>
2011-12-21 19:18:14 +0800

    Merging master

--- taskbook.py
+++ taskbook.py
@@ -6,35 +6,19 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
  
-class User(db.Model):
-    
-    name = db.StringProperty(required=True)
-    password = db.StringProperty(required=True)
-    email = db.StringProperty(required=True)
-    studentid = db.StringProperty(required=True)
-    college = db.IntegerProperty(required=True)
-    admin = db.BooleanProperty(required=True)
+class Greeting(db.Model):
+    author = db.UserProperty()
+    content = db.StringProperty(multiline=True)
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class College(db.Model):
-    name = db.StringProperty(required=True)
+    col_name = db.StringProperty()
 
 class Course(db.Model):
-    serialnumber = db.IntegerProperty(required=True)
-    name = db.StringProperty(required=True)
-    college = db.IntegerProperty(required=True)
-    professor = db.StringProperty()
-    room = db.StringProperty()
-    url = db.StringProperty()
+    cour_name = db.StringProperty(required=True)
+    cour_college = db.IntegerProperty(required=True)
+    cour_professor = db.StringProperty(required=True)
       
-class Resource(db.Model):
-    name = db.StringProperty(required=True)
-    course = db.IntegerProperty(required=True)
-    author = db.IntegerProperty(required=True)
-    resourcetype = db.StringProperty()
-    date = db.DateProperty(required=True)
-    description = db.StringProperty(multiline=True)
-    url = db.StringProperty(required=True)
-    dateUploaded = db.DateTimeProperty(auto_now_add=True)  
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -56,8 +40,40 @@ class MainPage(webapp.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'html/index.html')
         self.response.out.write(template.render(path, template_values))
 
+
+class AdminDB(webapp.RequestHandler):
+    def post(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'html/admindb.html')
+        self.response.out.write(template.render(path, template_values))
+
+class NewCollege(webapp.RequestHandler):
+    def post(self):
+        #college = College()
+        #college.name = self.request.get('name')
+        #college.put()
+        greeting = ""
+    
+        
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        greeting = Greeting()
+        #college = College()
+        #college.name = self.request.get('content')
+        #college.put();
+        
+        if users.get_current_user():
+            greeting.author = users.get_current_user()
+
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/')
+
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                      ('/sign', Guestbook),
+                                      ('/admindb',AdminDB),
+                                      ('/newCollege',NewCollege)],
                                      debug=True)
 
 def main():
