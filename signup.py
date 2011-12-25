147c01cac93bbd4001834ccb8f9544e1988f9d2b
t00901109 <antarex.rave@gmail.com>
2011-12-25 14:11:46 +0800

    usermodel created, signup completed, html improvements

--- signup.py
+++ signup.py
@@ -1,5 +1,6 @@
 import os
 from taskbook import College
+from taskbook import User
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -11,16 +12,31 @@ class MainPage(webapp.RequestHandler):
 
         path = os.path.join(os.path.dirname(__file__), 'html/signup.html')
         self.response.out.write(template.render(path, template_values))
-        
+
 class NewUser(webapp.RequestHandler):
-    def get(self):
-        title='title'
-        message='message'
+    def post(self):
+        user = User(name=self.request.get('name'),
+                    password=self.request.get('password'),
+                    email=self.request.get('email'),
+                    studentid=self.request.get('studentid'),
+                    college=College.get_by_id(int(self.request.get('college'))),
+                    admin=bool('')
+                    )
+        user.put()
+         
+        title='Thank you, ' + self.request.get('name')
+        message='The new user was successfully created. Now you can share your resources. Enjoy.'
+        method='get'
+        action='/'
+        value='continue'
         template_values = {'title': title,
-                           'message': message}
-        
+                           'message': message,
+                           'method': method,
+                           'action': action,
+                           'value': value}
+         
         path = os.path.join(os.path.dirname(__file__), 'html/message.html')
-        self.response.out.write(template.render(path, template_values)) 
+        self.response.out.write(template.render(path, template_values))
                 
 
  
